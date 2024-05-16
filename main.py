c75451a7de111529f2072476b3a32f3abc32f812
Shinusei <lyshagrishin@gmail.com>
2024-05-16 10:56:32 +0300

    new window open

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import sys
+
 from PyQt6 import QtCore, QtGui, QtWidgets
 from PyQt6 import uic
 from PyQt6.QtCore import QTimer, QTime, QDateTime
 from PyQt6.QtGui import QFont
-from PyQt6.QtWidgets import QTimeEdit
+from PyQt6.QtWidgets import QTimeEdit, QWidget, QVBoxLayout, QLabel
+
+
+class ChangeTime(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(ChangeTime, self).__init__(parent)
+        uic.loadUi("D_TilingTimeST.ui", self)
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -18,7 +25,11 @@ class MainWindow(QtWidgets.QMainWindow):
         current_time = QDateTime.currentDateTime()
         label_time = current_time.toString('''hh:mm:ss'\n'dd.MM.yyyy''')
         self.timeEdit.setText(label_time)
+        self.pushButton.clicked.connect(self.showChangeTime)
 
+    def showChangeTime(self):
+        self.w = ChangeTime()
+        self.w.show()
 
 
 app = QtWidgets.QApplication(sys.argv)
